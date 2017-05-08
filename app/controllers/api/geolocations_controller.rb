adda5740ab404ce951f00594db074507c4761033
Igor Springer <springerigor@gmail.com>
2017-05-08 22:07:34 +0200

    Implement a basic endpoint for fetching `Geolocation` records.

---
+++ app/controllers/api/geolocations_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  class GeolocationsController < ApiController
+    # TODO: Implement proper caching mechanism based on `updated_at` attribute
+    # and `stale?` Rails method to improve performance
+    def show
+      if geolocation
+        render geolocation
+      else
+        render json: { error: "Geolocation with IP #{ params[:id] } does not exist." }, status: 404
+      end
+    end
+
+    private
+
+    def geolocation
+      @geolocation ||= GeoImporter::Models::Geolocation.find(ip_address: params[:id])
+    end
+  end
+end
